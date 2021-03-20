2c6c9a118c58b9a53ccdb7c1f5d1b3df318cea16
Guilherme Luiz <guilhermemaia201450@gmail.com>
2021-03-20 11:44:11 -0300

    connect pipedrive

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,15 @@
 import { Router } from 'express';
 
+const lib = require('pipedrive');
+
+lib.Configuration.apiToken= '5e6debbcfb76027edcfd7bb656e73584dca6002d';
+
 const routes = Router();
+
+routes.get('/', async (req, res) => {
+    const user = await lib.UsersController.getCurrentUserData();
+ 
+    res.send(user);
+});
 
 export default routes;
