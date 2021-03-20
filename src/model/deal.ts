72453835b57f2445828daee613e5e03fa6565159
Guilherme Luiz <guilhermemaia201450@gmail.com>
2021-03-20 15:40:11 -0300

    pipedrive

---
+++ src/model/deal.ts
@@ -0,0 +1,7 @@
+interface deal{
+    title: string,
+    formatted_value: string,
+    status: string,
+    add_time: string,
+    update_time: string
+}
--- src/routes.ts
+++ src/routes.ts
@@ -4,12 +4,17 @@ const lib = require('pipedrive');
 
 lib.Configuration.apiToken= '5e6debbcfb76027edcfd7bb656e73584dca6002d';
 
+const controller = lib.DealsController;
+
 const routes = Router();
 
 routes.get('/', async (req, res) => {
     const user = await lib.UsersController.getCurrentUserData();
- 
-    res.send(user);
+    
+    const teste = await controller.getAllDeals({status: 'won'},function(error: any , response: any, context: any){
+        return context.data
+    });
+    res.send(teste.data);
 });
 
 export default routes;
